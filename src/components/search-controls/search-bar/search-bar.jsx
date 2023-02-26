8fbfa66337f8dc5f285db6e8654f0ad5962ffcb0
nickzh-17 <mikalayzh@gmail.com>
2023-02-26 20:57:09 +0300

    fix: search-icon onFocus color

--- src/components/search-controls/search-bar/search-bar.jsx
+++ src/components/search-controls/search-bar/search-bar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useWindowWidth } from '../../../functions/get-window-width';
 import { IconButton } from '../../ui/icon-button';
 import './search-bar.scss';
@@ -18,6 +18,8 @@ const useFocus = () => {
 }
 
 export const SearchBar = ({isSearchOpen, onSearchIconClick, query, setQuery}) => {
+    
+    const [isInputOnFocus, setIsInputOnFocus] = useState(false);
     const [inputRef, setInputFocus] = useFocus();
     useMountEffect( setInputFocus );
 
@@ -36,6 +38,12 @@ export const SearchBar = ({isSearchOpen, onSearchIconClick, query, setQuery}) =>
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd" d="M7.3335 2.66671C4.75617 2.66671 2.66683 4.75605 2.66683 7.33337C2.66683 9.9107 4.75617 12 7.3335 12C9.91083 12 12.0002 9.9107 12.0002 7.33337C12.0002 4.75605 9.91083 2.66671 7.3335 2.66671ZM1.3335 7.33337C1.3335 4.01967 4.01979 1.33337 7.3335 1.33337C10.6472 1.33337 13.3335 4.01967 13.3335 7.33337C13.3335 10.6471 10.6472 13.3334 7.3335 13.3334C4.01979 13.3334 1.3335 10.6471 1.3335 7.33337Z" fill="#A7A7A7"/>
                     <path fillRule="evenodd" clipRule="evenodd" d="M10.6284 10.6286C10.8887 10.3683 11.3108 10.3683 11.5712 10.6286L14.4712 13.5286C14.7315 13.789 14.7315 14.2111 14.4712 14.4714C14.2108 14.7318 13.7887 14.7318 13.5284 14.4714L10.6284 11.5714C10.368 11.3111 10.368 10.889 10.6284 10.6286Z" fill="#A7A7A7"/>
+                    <defs>
+                        <linearGradient id="searchGradient">
+                            <stop offset="5%" stopColor="#F60" />
+                            <stop offset="95%" stopColor="#FF6" />
+                        </linearGradient>
+                    </defs>
                 </svg>
             </IconButton>
 
@@ -70,13 +78,21 @@ export const SearchBar = ({isSearchOpen, onSearchIconClick, query, setQuery}) =>
     }
 
     return <div className='search-bar'>
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className={`search-icon ${ isInputOnFocus ? 'search-icon--focus' : '' }`} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M7.3335 2.66671C4.75617 2.66671 2.66683 4.75605 2.66683 7.33337C2.66683 9.9107 4.75617 12 7.3335 12C9.91083 12 12.0002 9.9107 12.0002 7.33337C12.0002 4.75605 9.91083 2.66671 7.3335 2.66671ZM1.3335 7.33337C1.3335 4.01967 4.01979 1.33337 7.3335 1.33337C10.6472 1.33337 13.3335 4.01967 13.3335 7.33337C13.3335 10.6471 10.6472 13.3334 7.3335 13.3334C4.01979 13.3334 1.3335 10.6471 1.3335 7.33337Z" fill="#A7A7A7"/>
             <path fillRule="evenodd" clipRule="evenodd" d="M10.6284 10.6286C10.8887 10.3683 11.3108 10.3683 11.5712 10.6286L14.4712 13.5286C14.7315 13.789 14.7315 14.2111 14.4712 14.4714C14.2108 14.7318 13.7887 14.7318 13.5284 14.4714L10.6284 11.5714C10.368 11.3111 10.368 10.889 10.6284 10.6286Z" fill="#A7A7A7"/>
+            <defs>
+                <linearGradient id='searchGradient' gradientUnits='userSpaceOnUse' x1='106.35%' y1='8.65%' x2='-6.35%' y2='91.35%'>
+                    <stop offset='50%' stopColor='#F83600'/>
+                    <stop offset='99%' stopColor='#F9D423'/>
+                </linearGradient>
+            </defs>
         </svg>
         <input
             data-test-id='input-search'
             value={query} 
+            onFocus={ () => setIsInputOnFocus(true)} 
+            onBlur={ () => setIsInputOnFocus(false) }
             onChange={event => setQuery(event.target.value)}
             type="text" 
             placeholder='Поиск книги или автора…' 
